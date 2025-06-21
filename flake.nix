6190af6364c6433523f0820056a730bed700be1f
Niko Matsakis <niko@alum.mit.edu>
2025-06-21 01:04:28 +0000

    Remove unnecessary aliases for cleaner environment
    - Remove random aliases like ga, la, ll, gc, gp, gl, etc.
    - Remove ls/cat/grep/find replacements - use original commands
    - Keep only essential vim keybindings and environment setup
    - Focus on core functionality without alias clutter
    - Maintain clean, minimal shell experience

--- flake.nix
+++ flake.nix
@@ -57,21 +57,6 @@
           ];
           
           shellHook = ''
-            # Set up aliases
-            alias ll='eza -la'
-            alias la='eza -a'
-            alias ls='eza'
-            alias cat='bat'
-            alias find='fd'
-            alias grep='rg'
-            
-            # Git aliases
-            alias gs='git status'
-            alias ga='git add'
-            alias gc='git commit'
-            alias gp='git push'
-            alias gl='git log --oneline'
-            
             # Set preferred editor and shell
             export EDITOR=vim
             export VISUAL=vim
@@ -106,19 +91,6 @@ zle -N zle-keymap-select
 
 # Initialize cursor (silent)
 echo -ne '\e[5 q' >/dev/tty 2>/dev/null
-
-# Load aliases
-alias ll='eza -la'
-alias la='eza -a'
-alias ls='eza'
-alias cat='bat'
-alias find='fd'
-alias grep='rg'
-alias gs='git status'
-alias ga='git add'
-alias gc='git commit'
-alias gp='git push'
-alias gl='git log --oneline'
 
 # Environment
 export EDITOR=vim
@@ -227,25 +199,6 @@ EOF
             export EDITOR=vim
             export VISUAL=vim
             export RUST_BACKTRACE=1
-            
-            # Modern CLI aliases (when tools are available)
-            command -v eza >/dev/null && alias ls='eza' && alias ll='eza -la' && alias la='eza -a'
-            command -v bat >/dev/null && alias cat='bat'
-            command -v rg >/dev/null && alias grep='rg'
-            command -v fd >/dev/null && alias find='fd'
-            
-            # Git aliases
-            alias gs='git status'
-            alias ga='git add'
-            alias gc='git commit'
-            alias gp='git push'
-            alias gl='git log --oneline'
-            alias gd='git diff'
-            
-            # Useful aliases
-            alias ..='cd ..'
-            alias ...='cd ../..'
-            alias l='ls -CF'
             
             # History settings
             HISTSIZE=10000
