a8daa44e1e9d664e5e72340a3a8925c30ec98b46
Niko Matsakis <niko@alum.mit.edu>
2025-06-23 21:23:49 -0400

    include imagemagick

--- flake.nix
+++ flake.nix
@@ -56,6 +56,9 @@
             # Network tools
             openssh
             rsync
+            
+            # Image processing
+            imagemagick
           ];
           
           shellHook = ''
